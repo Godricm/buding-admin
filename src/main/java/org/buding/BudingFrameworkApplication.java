b31fe3d27c0750f9fbe645a4bba303ac379d4b87
Godric <miaochenGod@outlook.com>
2019-06-14 16:33:12 +0800

    [update] 基础文件更新

--- src/main/java/org/buding/BudingFrameworkApplication.java
+++ src/main/java/org/buding/BudingFrameworkApplication.java
@@ -1,6 +1,5 @@
 package org.buding;
 
-import main.java.org.buding.dto.BaseUser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
