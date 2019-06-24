fe3f4001f4577b444cef65b02d8ef48e84d23f6c
godric <miaochenGod@outlook.com>
2019-06-24 16:43:21 +0800

    [add] 新增selectByLoginName方法

--- src/main/java/org/buding/dao/BaseUserMapper.java
+++ src/main/java/org/buding/dao/BaseUserMapper.java
@@ -4,4 +4,5 @@ import org.buding.dto.BaseUser;
 import org.buding.util.BaseMapper;
 
 public interface BaseUserMapper extends BaseMapper<BaseUser> {
+    BaseUser selectByLoginName(String loginName);
 }
